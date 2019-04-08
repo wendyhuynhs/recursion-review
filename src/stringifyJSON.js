5a3e710e625cdef36f3c08ff3793a86f0fb88854
wendyhuynhs <wendyhuynhs@gmail.com>
2019-04-08 11:35:01 -0700

    Completed stringifyJSON and passed all test

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,39 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  // find type of input (array/obj/str)
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'boolean') {
+    return obj ? "true" : "false";
+  }
+  if (typeof obj === 'string') {
+    return '"'+ obj +'"';
+  }
+  if (Array.isArray(obj)) {
+    let results = [];
+    obj.forEach(function(item) {
+      results.push(stringifyJSON(item));
+    })
+    return '[' + results.join(',') + ']';
+  }
+  if(typeof obj === "object") {
+    let result = [];
+    for(let key in obj){
+      if (typeof obj[key] === 'function') {
+        return '{}';
+      }
+     let chunk = "";
+     chunk = '"' + key.toString() + '":' + stringifyJSON(obj[key]);
+     result.push(chunk);
+    }
+    return '{' + result.join(",") + '}';
+  }
+
+  
 };
